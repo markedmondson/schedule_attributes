f5fee3083c7faa96b676ca49ad7cfe98f5ba79c1
Mark Edmondson <mark.edmondson@gmail.com>
2019-06-06 21:41:11 -0700

    Remove opinionated form builder

--- lib/schedule_attributes.rb
+++ lib/schedule_attributes.rb
@@ -1,6 +1,5 @@
 require 'schedule_attributes/model'
 require 'schedule_attributes/railtie' if defined? Rails
-require 'schedule_attributes/form_builder' if defined? Formtastic
 
 if defined? Arel
   module Arel::Visitors
